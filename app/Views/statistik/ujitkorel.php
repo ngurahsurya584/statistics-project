01bf77e55edab2e3806d0ffa885300b23be6ea20
ngurahsurya584 <ngurahgusti584@gmail.com>
2021-07-20 05:44:53 +0800

    the_project

---
+++ app/Views/statistik/ujitkorel.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/template1'); ?>
+
+<?= $this->Section('content'); ?>
+<section id="jumbo" class="jumbo">
+    <div class="container">
+        <div class="jumbotron jumbotron-fluid mt-5">
+            <div class="container text-center">
+                <h4 id="kalimat" class="text-dark text-center">Chi Kuadrat</h4>
+            </div>
+
+
+            <?= $this->endSection(); ?>
